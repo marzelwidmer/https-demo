bfe30f524f2ccd0733b22fcf2d7e1d62b0e54b0e
marzelwidmer <marzelwidmer@gmail.com>
2020-11-29 15:12:07 +0100

    Upgrad Spring Boot Version (2.4.0 with compatible Spring Cloud Version 2020.0.0-M5)

--- kboot-demo-https-client/build.gradle.kts
+++ kboot-demo-https-client/build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	//id("org.springframework.boot") version "2.4.0"
-	id("org.springframework.boot") version "2.3.4.RELEASE"
+	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
@@ -15,7 +14,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 
-extra["springCloudVersion"] = "Hoxton.SR9"
+extra["springCloudVersion"] = "2020.0.0-M5"
 
 springBoot {
 	buildInfo()
@@ -27,6 +26,7 @@ repositories {
 	jcenter()
 	maven {
 		setUrl("https://dl.bintray.com/arrow-kt/arrow-kt/")
+		setUrl("https://repo.spring.io/milestone")
 	}
 }
 
